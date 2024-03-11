12e573b3153d86e3c37cb9aa626ccb0dd35334a9
markush0f <abramianmedina@gmail.com>
2024-03-11 19:54:57 +0000

    url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("members.urls")),
-    path("api/", include("league.urls")),
+    # path("api/", include("league.urls")),
 ]
