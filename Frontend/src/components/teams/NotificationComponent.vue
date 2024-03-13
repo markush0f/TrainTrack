c88f4ed8e6e4b518a254e61f9d0fdecd1dc4f82d
Jose Manuel <prueba@gmail.com>
2024-03-13 17:07:16 +0000

    Cambios a componentes

---
+++ Frontend/src/components/teams/NotificationComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="justify-center p-4 border border-gray-400">  
+            <h2 class="text-center font-bold mb-4">Notificaciones</h2>
+        <div class="w-full p-4 max-h-60 overflow-y-auto">
+
+      <div class="bg-gray-100 p-4 rounded-lg">
+        <!-- Contenido de las notificaciones -->
+        <!-- Aquí se mostrarían las notificaciones -->
+        <div class="mb-4">Notificación 1</div>
+        <div class="mb-4">Notificación 2</div>
+        <div class="mb-4">Notificación 3</div>
+        <div class="mb-4">Notificación 4</div>
+        <div class="mb-4">Notificación 5</div>
+        <div class="mb-4">Notificación 6</div>
+        <div class="mb-4">Notificación 7</div>
+        <div class="mb-4">Notificación 8</div>
+        <div class="mb-4">Notificación 9</div>
+        <div class="mb-4">Notificación 10</div>
+        <!-- Fin del contenido de las notificaciones -->
+      </div>
+    </div>
+
+      </div>
+</template>
--- Frontend/src/views/TrainerView.vue
+++ Frontend/src/views/TrainerView.vue
@@ -15,58 +15,11 @@
     </div>
     <!-- div que recoge horarios de entrenamiento, ultimas noticias y mensajes a los padres -->
     <div>
-      <!-- div de aviso de los entrenamientos -->
-      <div class="justify-center p-4 border border-gray-400">  
-        <h1 class="mb-4 text-4xl font-calibri leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black text-center">Horarios de entrenamiento</h1>
-        <table class="w-full border-collapse border border-gray-200">
-  <thead>
-    <tr>
-      <th class="px-4 py-2 bg-green-500 border border-gray-200">L</th>
-      <th class="px-4 py-2 bg-green-500 border border-gray-200">M</th>
-      <th class="px-4 py-2 bg-green-500 border border-gray-200">X</th>
-      <th class="px-4 py-2 bg-green-500 border border-gray-200">J</th> 
-      <th class="px-4 py-2 bg-green-500 border border-gray-200">V</th>
-      <th class="px-4 py-2 bg-red-300 border border-gray-200">S</th>
-      <th class="px-4 py-2 bg-red-300 border border-gray-200">D</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr class="text-center">
-      <td class="px-4 py-2 bg-green-300 border border-gray-200">F</td>
-      <td class="px-4 py-2 bg-blue-300 border border-gray-200">19-21</td>
-      <td class="px-4 py-2 bg-green-300 border border-gray-200">F</td>
-      <td class="px-4 py-2 bg-blue-300 border border-gray-200">19-21</td>
-      <td class="px-4 py-2 bg-green-300 border border-gray-200">F</td>
-      <td class="px-4 py-2 bg-yellow-300 border border-gray-200">MD</td>
-      <td class="px-4 py-2 bg-green-300 border border-gray-200">F</td>
-    </tr>
-  </tbody>
-</table>
-
-      </div>
+      <!-- Calendario de entrenamiento-->
+      <CalendariComponent />
       <!-- div de ultimas notificaciones -->
-      <div class="justify-center p-4 border border-gray-400">  
-            <h2 class="text-center font-bold mb-4">Notificaciones</h2>
-        <div class="w-full p-4 max-h-60 overflow-y-auto">
-
-      <div class="bg-gray-100 p-4 rounded-lg">
-        <!-- Contenido de las notificaciones -->
-        <!-- Aquí se mostrarían las notificaciones -->
-        <div class="mb-4">Notificación 1</div>
-        <div class="mb-4">Notificación 2</div>
-        <div class="mb-4">Notificación 3</div>
-        <div class="mb-4">Notificación 4</div>
-        <div class="mb-4">Notificación 5</div>
-        <div class="mb-4">Notificación 6</div>
-        <div class="mb-4">Notificación 7</div>
-        <div class="mb-4">Notificación 8</div>
-        <div class="mb-4">Notificación 9</div>
-        <div class="mb-4">Notificación 10</div>
-        <!-- Fin del contenido de las notificaciones -->
-      </div>
-    </div>
-
-      </div>
+      <NotificationComponent />
+      
       <!-- div de mensajes a los padres -->
       <h1 class="text-3xl justify-center">Enviar mensaje al padre del jugador</h1>
         <div class="flex p-4 border border-gray-400">
@@ -115,4 +68,6 @@
 <script setup>
 import NavbarComponent from '@/components/layouts/NavbarComponent.vue';
 import ClassificationTableComponent from '@/components/teams/ClassificationTableComponent.vue';
+import NotificationComponent from '@/components/teams/NotificationComponent.vue';
+import CalendariComponent from '@/components/teams/CalendariComponent.vue';
 </script>
