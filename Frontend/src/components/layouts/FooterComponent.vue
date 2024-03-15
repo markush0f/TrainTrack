3b7e06f3acef8e45a1174c6df828a0ac933d09a7
Jose Manuel <prueba@gmail.com>
2024-03-15 16:22:41 +0000

    footer

--- Frontend/src/components/layouts/FooterComponent.vue
+++ Frontend/src/components/layouts/FooterComponent.vue
@@ -1,9 +1,19 @@
 <template>
-    <footer class="bg-white-800 text-black p-2 fixed inset-x-0 bottom-0">
+    <footer class="bg-black-800 text-black p-2 fixed inset-x-0 bottom-0">
   <div class="max-w-6xl mx-auto px-4">
     <div class="flex justify-between">
-      <div>
-        <a href="#" class="hover:text-black-300">Social Network</a>
+      <div class="flex item-center">
+        <div class="flex items-center space-x-">
+            <a href="#" class="hover:text-black-300">
+              <img src="../img/instagram.png">
+            </a>
+            <a href="#" class="hover:text-black-300 p-2">
+              <i class="material-icons">facebook</i>
+            </a>
+            <a href="#" class="hover:text-black-300">
+              
+            </a>
+        </div>
       </div>
       <div>
         <a href="#" class="hover:text-black-300">Services</a>
@@ -15,7 +25,7 @@
   </div>
 </footer>
 </template>
-
+ 
 <script setup>
 
 </script>
--- Frontend/src/components/layouts/NavbarComponent.vue
+++ Frontend/src/components/layouts/NavbarComponent.vue
@@ -5,26 +5,24 @@
         <span class="text-3xl font-Blacker">Logo</span>
       </div>
       <!-- Links -->
-      <div class="flex-1 flex justify-center">
+      <div class="flex-1 flex justify-center space-x-2">
         <!-- Rutas de prueba -->
-        <router-link to="/" class="text-base px-3 py-2 hover:text-main-green">INICIO</router-link>
+        <router-link to="/" class="text-base px-3 py-2 hover:text-main-green"><i class="material-icons text-4xl">home</i></router-link>
         <!-- <button @click="logout" class="text-base px-3 py-2 hover:text-main-green">Logout</button> -->
         <!-- CONDICION PARA SI ESTÁ REGISTRADO MOSTRAR SU PERFIL -->
       </div>
-      <button @click=" profileStore.show = true" class="text-xl px-3 py-2 hover:text-main-green text-rigth">
-        Perfil
-    </button>
+      <div class="flex item-center space-x-2">
+        <button @click=" profileStore.show = true"><i class="material-icons text-4xl">person</i></button>
+      </div>
       <Modal v-if="profileStore.show" @close="profileStore.show = false" />
     </div>
 
   </nav>
 </template>
 <script setup>
-
 import axios from 'axios';
 import { ref } from 'vue'
 import Modal from '@/components/Modal.vue';
 const profileStore = useProfileStore();
 import { useProfileStore } from '@/stores/profile';
-
 </script>
